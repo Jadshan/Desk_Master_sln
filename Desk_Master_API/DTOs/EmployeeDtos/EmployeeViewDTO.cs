94f967a3764949cfa2bd71cb497453c60c31f850
Jdshan <mjadshan@gmail.com>
2024-03-03 10:00:29 +0530

    set DTOs

---
+++ Desk_Master_API/DTOs/EmployeeDtos/EmployeeViewDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desk_Master_API.DTOs.EmployeeDtos
+{
+    public class EmployeeViewDTO
+    {
+        public int Id { get; set; } 
+        public string EmpName { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
+        public decimal Salary { get; set; }
+    }
+}
