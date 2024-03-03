84dc2de938200e090f0212eca688acd13dc0e4d1
Jdshan <mjadshan@gmail.com>
2024-03-03 12:58:00 +0530

    update contact details table address column

--- Desk_Master_API/Models/ContactDetail.cs
+++ Desk_Master_API/Models/ContactDetail.cs
@@ -8,7 +8,7 @@ namespace Desk_Master_API.Models
     public class ContactDetail
     {
         public int Id { get; set; }
-        public long Adress { get; set; }
+        public string Address { get; set; } = string.Empty;
         public string PhoneNo { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public int? EmployeeId { get; set; }
