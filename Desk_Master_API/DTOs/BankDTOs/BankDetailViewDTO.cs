d19d093de457635428e0653a34634f23f63ed4a9
Jdshan <mjadshan@gmail.com>
2024-03-04 13:28:09 +0530

    add bank data

---
+++ Desk_Master_API/DTOs/BankDTOs/BankDetailViewDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desk_Master_API.DTOs
+{
+    public class BankDetailViewDTO
+    {
+        public int Id { get; set; }
+        public string BankName { get; set; } = string.Empty;
+        public string Branch { get; set; } = string.Empty;
+        public string AccountNo { get; set; } = string.Empty;
+        public int? EmployeeId { get; set; }
+    }
+}
---
+++ Desk_Master_API/Mappers/BankDetailMapper.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Desk_Master_API.DTOs;
+using Desk_Master_API.DTOs.BankDTOs;
+using Desk_Master_API.DTOs.ContactDtos;
+using Desk_Master_API.Models;
+
+namespace Desk_Master_API.Mappers
+{
+    public static class BankDetailMapper
+    {
+        public static BankDetailViewDTO ToBankDetailViewDTO(this BankDetail bankDetail)
+        {
+            return new BankDetailViewDTO
+            {
+                Id = bankDetail.Id,
+                BankName = bankDetail.BankName,
+                Branch = bankDetail.Branch,
+                AccountNo = bankDetail.AccountNo,
+                EmployeeId = bankDetail.EmployeeId
+            };
+        }
+              public static BankDetail ToBankDetailFromAddDTO(this AddBankRequestDTO requestDTO, int EmployId)
+        {
+            return new BankDetail
+            {
+              BankName = requestDTO.BankName,
+              Branch= requestDTO.Branch,
+              AccountNo = requestDTO.AccountNo,
+              EmployeeId = EmployId
+            };
+        }
+    }
+}
