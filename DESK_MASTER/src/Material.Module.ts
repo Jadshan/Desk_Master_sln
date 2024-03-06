91e98532a17810f12c89f5a3befce982fa241c1f
Jdshan <mjadshan@gmail.com>
2024-03-06 22:50:31 +0530

    update

--- DESK_MASTER/src/Material.Module.ts
+++ DESK_MASTER/src/Material.Module.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+
 import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -22,7 +22,7 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+
 import { MatListModule } from '@angular/material/list';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -35,7 +35,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
-    FormsModule,
+
     MatIconModule,
     MatTableModule,
     MatSelectModule,
@@ -52,7 +52,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
     MatListModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    NgxMatTimepickerModule,
+
     MatMomentDateModule,
     MatChipsModule,
     MatButtonToggleModule,
