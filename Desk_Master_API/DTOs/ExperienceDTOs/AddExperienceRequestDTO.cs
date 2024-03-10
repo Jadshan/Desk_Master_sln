842ede5d308325af1c541ff992bfe3e40240d643
Jdshan <mjadshan@gmail.com>
2024-03-10 23:56:52 +0530

    experience dto

---
+++ Desk_Master_API/DTOs/ExperienceDTOs/AddExperienceRequestDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desk_Master_API.DTOs.ExperienceDTOs
+{
+    public class AddExperienceRequestDTO
+    {
+       public string Company { get; set; } = string.Empty;
+       public string StartDate { get; set; } = string.Empty;
+       public string EndDate { get; set; } = string.Empty;
+       public string Designation { get; set; } = string.Empty;
+       public string Project { get; set; } = string.Empty;
+    }
+}
