cfcb606480e001fd82ddbce015af85d0946f0ac1
Jdshan <mjadshan@gmail.com>
2024-03-19 07:53:25 +0530

    set AddTimeAllocationDTO

---
+++ Desk_Master_API/DTOs/InterviewDTOs/AddTimeAllocationDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desk_Master_API.DTOs.InterviewDTOs
+{
+    public class AddTimeAllocationDTO
+    {
+        public string StartTime { get; set; } = string.Empty;
+        public string EndTime { get; set; } = string.Empty;
+        public int TimeSlotRange { get; set; } = 0;
+    }
+}
