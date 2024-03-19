01c533f90775a2763841291fd61c244575943efe
Jdshan <mjadshan@gmail.com>
2024-03-19 07:58:25 +0530

    set employData

--- Desk_Master_App/src/app/shared/store/App.state.ts
+++ Desk_Master_App/src/app/shared/store/App.state.ts
@@ -4,12 +4,16 @@ import { userReducer } from '../../apps/user-management/+store/user.reducer';
 import { authReducer } from '../../auth/+Store/user.reducer';
 import { schedularReducer } from '../../apps/scheduler/+store/schedular.reducer';
 import { hrReducer } from '../../apps/hr-view/+store/hr.reducer';
-import { employeeReducer } from '../../apps/employ-management/+Store/Employee/employee.reducer';
+import {
+  employeeDataReducer,
+  employeeReducer,
+} from '../../apps/employ-management/+Store/Employee/employee.reducer';
 
 export const AppState = {
   blog: blogReducer,
   app: AppReducer,
   employ: employeeReducer,
+  employData: employeeDataReducer,
   user: userReducer,
   auth: authReducer,
   interview: hrReducer,
