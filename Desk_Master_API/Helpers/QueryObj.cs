b7926e9d9e654654851ec0cf01229e774093873d
Jdshan <mjadshan@gmail.com>
2024-03-24 08:59:19 +0530

    QueryObj

---
+++ Desk_Master_API/Helpers/QueryObj.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desk_Master_API.Helpers
+{
+    public class QueryObj
+    {
+        public string? CandidateName { get; set; } 
+    }
+}
