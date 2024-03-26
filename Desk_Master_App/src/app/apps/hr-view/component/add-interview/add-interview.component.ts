dddb916850958861c0f29141b437b6551bbe31b4
Jdshan <mjadshan@gmail.com>
2024-03-26 19:39:03 +0530

    code refactoring

--- Desk_Master_App/src/app/apps/hr-view/component/add-interview/add-interview.component.ts
+++ Desk_Master_App/src/app/apps/hr-view/component/add-interview/add-interview.component.ts
@@ -1,93 +1,178 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import moment from 'moment';
-import { Interview, TimeSlot } from '../../+store/Model';
-import { HrService } from '../../service/hr.service';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { IInterview, TimeSlot } from '../../+store/Model';
 import {
   addInterview,
-  loadTimeSlot,
+  loadInterviewBoard,
+  loadTimeAllocation,
   updateInterview,
 } from '../../+store/hr.action';
 import {
   getInterviewById,
-  getTimeSlotList,
+  getInterviewersListByDate,
   getTimeSlotListByDate,
 } from '../../+store/hr.selector';
 import { DateAdapter } from '@angular/material/core';
+import { IInterviewer } from '../../../employ-management/+Store/Model/employee.model';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import moment from 'moment';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+
 @Component({
   selector: 'app-add-interview',
   templateUrl: './add-interview.component.html',
   styleUrls: ['./add-interview.component.css'],
 })
 export class AddInterviewComponent implements OnInit {
-  add($event: Event) {
-    throw new Error('Method not implemented.');
-  }
-  title: string = 'Schedule New Interview';
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  interviewerCtrl = new FormControl('');
+  title = 'Schedule New Interview';
   interviewForm!: FormGroup;
   interviewDate: any = new Date();
   timeList: TimeSlot[] = [];
   editingInterviewTime!: string;
-  buttonStatus: string = 'Add New';
+  buttonStatus = 'Add New';
+  interviewersForDate: IInterviewer[] = [];
+  timeSlots: string[] = [];
+  interviewers: IInterviewer[] = [];
+  @ViewChild('interviewerInput')
+  interviewerInput!: ElementRef<HTMLInputElement>;
+  filteredInterviewers$: Observable<IInterviewer[]> | undefined;
+  editedInterviewDate = '';
   constructor(
     private formBuilder: FormBuilder,
-    private service: HrService,
     private dialogRef: MatDialogRef<AddInterviewComponent>,
     private store: Store,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dateAdapter: DateAdapter<Date>
   ) {
     this.dateAdapter.setLocale('en-GB');
+    this.title = this.data.title;
+    this.initializeForm();
+    this.filteredInterviewers$ = of([]);
+  }
+
+  ngOnInit(): void {
+    const selectedDate = moment(this.interviewDate).format('MMM DD, YYYY');
+    this.loadTimeSlots(selectedDate);
+    this.updateForm();
+  }
+
+  initializeForm(): void {
     this.interviewForm = this.formBuilder.group({
       candidateName: ['', Validators.required],
       candidateEmail: ['', [Validators.required, Validators.email]],
       candidatePhoneNo: ['', Validators.required],
       date: [this.interviewDate, Validators.required],
-      timeSlot: [this.timeList[0], Validators.required],
-      interviewer: ['', Validators.required],
+      timeSlot: [this.timeSlots[0], Validators.required],
+      interviewer: [''],
       additionalInfo: [''],
     });
   }
-  ngOnInit(): void {
+
+  // updateForm(): void {
+  //   if (this.title != 'Add New') {
+  //     this.buttonStatus = this.data.title;
+  //     this.store
+  //       .select(getInterviewById(this.data.id))
+  //       .subscribe((_editedInterview) => {
+  //         this.interviewForm.setValue({
+  //           candidateName: _editedInterview.candidateName,
+  //           candidateEmail: _editedInterview.candidateEmail,
+  //           candidatePhoneNo: _editedInterview.candidatePhoneNo,
+  //           date: new Date(_editedInterview.date),
+  //           timeSlot: _editedInterview.time,
+  //           additionalInfo: _editedInterview.additionalInfo,
+  //         });
+  //         this.interviewDate = new Date(_editedInterview.date);
+  //         this.editingInterviewTime = _editedInterview.time;
+  //         this.initializeInterviewers(
+  //           moment(_editedInterview.date).format('MMM DD, YYYY')
+  //         );
+  //         this.interviewers = _editedInterview.interviewersList;
+  //       });
+  //   }
+  // }
+
+  initializeInterviewers(selectedDate: string): void {
+    this.store.dispatch(loadInterviewBoard());
+    this.store
+      .select(getInterviewersListByDate(selectedDate))
+      .subscribe((interviewers) => {
+        this.interviewersForDate = interviewers;
+        this.filteredInterviewers$ = this.interviewForm
+          .get('interviewer')
+          ?.valueChanges.pipe(
+            startWith(null),
+            map((value: string | null) =>
+              value
+                ? this._filterInterviewers(value)
+                : this.interviewersForDate.slice()
+            )
+          );
+      });
+  }
+
+  loadTimeSlots(selectedDate: string): void {
     this.title = this.data.title;
-    if (this.title != 'Add New') {
+    this.initializeInterviewers(selectedDate);
+    this.store.dispatch(loadTimeAllocation());
+    this.store
+      .select(getTimeSlotListByDate(selectedDate))
+      .subscribe((timeSlots) => {
+        this.timeSlots = timeSlots;
+        if (this.title === 'Add New' && this.timeSlots.length > 0) {
+          this.interviewForm.patchValue({
+            timeSlot: this.timeSlots[0],
+          });
+        } else {
+          this.timeSlots.push(this.editingInterviewTime);
+          this.interviewForm.patchValue({
+            timeSlot: this.timeSlots[this.timeSlots.length - 1],
+          });
+        }
+      });
+  }
+
+  updateForm(): void {
+    if (this.title !== 'Add New') {
       this.buttonStatus = this.data.title;
       this.store
         .select(getInterviewById(this.data.id))
-        .subscribe((_editedInterview) => {
-          this.interviewForm.setValue({
-            candidateName: _editedInterview.candidateName,
-            candidateEmail: _editedInterview.candidateEmail,
-            candidatePhoneNo: _editedInterview.candidatePhoneNo,
-            date: new Date(_editedInterview.date),
-            timeSlot: _editedInterview.timeSlot,
-            interviewer: _editedInterview.interviewer,
-            additionalInfo: _editedInterview.additionalInfo,
-          });
-          this.interviewDate = new Date(_editedInterview.date);
-          this.editingInterviewTime = _editedInterview.timeSlot;
-        });
-    }
-    this.store.dispatch(loadTimeSlot());
-    const selectedDate = moment(this.interviewDate).format('MMM DD, YYYY');
-    this.store.select(getTimeSlotListByDate(selectedDate)).subscribe((time) => {
-      this.timeList = time;
-      if (this.title == 'Add New') {
-        if (this.timeList.length > 0) {
+        .subscribe((editedInterview) => {
           this.interviewForm.patchValue({
-            timeSlot: this.timeList[0].timeSlot,
+            candidateName: editedInterview.candidateName,
+            candidateEmail: editedInterview.candidateEmail,
+            candidatePhoneNo: editedInterview.candidatePhoneNo,
+            date: new Date(editedInterview.date),
+            timeSlot: editedInterview.time,
+            additionalInfo: editedInterview.additionalInfo,
           });
-        }
-      } else {
-        this.timeList.push({ id: 'edit', timeSlot: this.editingInterviewTime });
-        this.interviewForm.patchValue({
-          timeSlot: this.timeList[this.timeList.length - 1].timeSlot,
+          this.editingInterviewTime = editedInterview.time;
+          this.interviewers =
+            editedInterview.interviewersList as IInterviewer[];
+          this.editedInterviewDate = moment(editedInterview.date).format(
+            'MMM DD, YYYY'
+          );
         });
-      }
-    });
-    console.log(this.timeList);
+      this.loadTimeSlots(this.editedInterviewDate);
+      this.initializeInterviewers(this.editedInterviewDate);
+    }
   }
 
   onDateSelected(event: any): void {
@@ -95,42 +180,72 @@ export class AddInterviewComponent implements OnInit {
     if (momentDate) {
       this.interviewDate = momentDate.format('MMM DD, YYYY');
     }
-    this.store
-      .select(getTimeSlotListByDate(this.interviewDate))
-      .subscribe((time) => {
-        // Call getTimeSlotListByDate with the initial interviewDate
-        this.timeList = time;
-        if (this.timeList.length > 0) {
-          this.interviewForm.patchValue({
-            timeSlot: this.timeList[0].timeSlot,
-          });
-        }
-      });
+    this.initializeInterviewers(this.interviewDate);
+    this.loadTimeSlots(this.interviewDate);
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.interviewers = [...this.interviewers, event.option.value];
+
+    this.interviewerInput.nativeElement.value = '';
+    this.interviewForm.get('interviewer')?.setValue(null);
+    const index = this.interviewersForDate.indexOf(event.option.value);
+    if (index !== -1) {
+      this.interviewersForDate.splice(index, 1);
+      // Reassign the filteredInterviewers$ observable with the updated interviewersForDate
+      this.filteredInterviewers$ = of(this.interviewersForDate.slice());
+    }
+  }
+
+  remove(interviewer: IInterviewer): void {
+    const index = this.interviewers.indexOf(interviewer);
+    if (index >= 0) {
+      this.interviewers = this.interviewers.filter(
+        (i) => i.employeeId !== interviewer.employeeId
+      );
+    }
+    this.filteredInterviewers$?.subscribe((interviewers) => {
+      interviewers.push(interviewer);
+      this.filteredInterviewers$ = of(interviewers);
+    });
+  }
+
+  private _filterInterviewers(value: string): IInterviewer[] {
+    if (!value || typeof value !== 'string') {
+      return [];
+    }
+    const filterValue = value.toLowerCase();
+    return this.interviewersForDate.filter((interviewer) =>
+      interviewer.name.toLowerCase().includes(filterValue)
+    );
   }
 
   onSubmit(): void {
     if (this.interviewForm.valid) {
-      const _interviewData: Interview = {
+      const _interviewData: IInterview = {
         candidateName: this.interviewForm.value.candidateName,
         candidateEmail: this.interviewForm.value.candidateEmail,
         candidatePhoneNo: this.interviewForm.value.candidatePhoneNo,
-        date: moment(this.interviewForm.value.date).format('MMM DD, YYYY'),
-        timeSlot: this.interviewForm.value.timeSlot,
-        interviewer: this.interviewForm.value.interviewer,
+        date: this.interviewForm.value.date.toISOString(),
+        time: this.interviewForm.value.timeSlot,
+        interviewersList: this.interviewers,
         additionalInfo: this.interviewForm.value.additionalInfo,
-        status: true,
+        status: '',
       };
-      if (this.title != 'Add New') {
+      console.log(_interviewData);
+
+      if (this.title !== 'Add New') {
         this.store.dispatch(
           updateInterview({ interviewData: _interviewData, id: this.data.id })
         );
       } else {
         this.store.dispatch(addInterview({ interviewData: _interviewData }));
       }
-      this.ClosePopup();
+      this.closePopup();
     }
   }
-  ClosePopup() {
+
+  closePopup(): void {
     this.dialogRef.close();
   }
 }
