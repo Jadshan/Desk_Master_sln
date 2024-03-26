8a4920635d4d553484b3e9169db1db867375defe
Jdshan <mjadshan@gmail.com>
2024-03-26 19:30:09 +0530

    change view

--- Desk_Master_API/DTOs/InterviewDTOs/InterviewersListViewDTO.cs
+++ Desk_Master_API/DTOs/InterviewDTOs/InterviewersListViewDTO.cs
@@ -4,10 +4,11 @@ namespace Desk_Master_API.DTOs.InterviewDTOs
 {
     public class InterviewersListViewDTO
     {
-        public int Id { get; set; }
+        //public int? Id { get; set; }
+         public int? EmployeeId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
-        public int? EmployeeId { get; set; } 
-        public int? InterviewId { get; set; } 
+        
+        //public int? InterviewId { get; set; } 
     }
 }
